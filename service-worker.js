7e942ab926b2bf59534546e8221b1b2363155b30
Nithin-DL <73882433+Nithin-DL@users.noreply.github.com>
2025-09-08 09:59:16 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
-const CACHE_NAME = "ipo-cache-v1";
+const CACHE_NAME = "ipo-pwa-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+  "index.html",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
-// Install Service Worker
+// Install event
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -14,11 +15,24 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// Fetch from Cache
+// Fetch event
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
     })
   );
 });
